4b7ab193573ea6cd6e3f600cbcf574153b44dfe6
DESKTOP-1AMS4RR\Silvio Fankhauser <silviofankhauser@gmail.com>
2017-03-04 19:47:06 +0100

    File Writer

--- src/main/java/ch/uzh/ase/What2Think.java
+++ src/main/java/ch/uzh/ase/What2Think.java
@@ -1,15 +1,48 @@
 package ch.uzh.ase;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class What2Think {
 
     public static void main(String[] args) {
+
         String topic = "Penrith";
-        ArrayList<String> tweets = TweetManager.getTweets(topic);
-        NLP.init();
-        for(String tweet : tweets) {
-            System.out.println(tweet + " : " + NLP.findSentiment(tweet));
+        ArrayList<String> tweets = null;
+
+        File file = new File("tweets.txt");
+        final FileWriter fw;
+        final BufferedWriter bw;
+
+        try {
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            fw = new FileWriter(file.getAbsoluteFile());
+            bw = new BufferedWriter(fw);
+            tweets = TweetManager.getTweets(topic);
+
+            for(String tweet : tweets) {
+                bw.write(tweet);
+                bw.newLine();
+            }
+
+            bw.flush();
+            bw.close();
+            fw.close();
+
+            NLP.init();
+
+            for(String tweet : tweets) {
+                System.out.println(tweet + " : " + NLP.findSentiment(tweet));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
