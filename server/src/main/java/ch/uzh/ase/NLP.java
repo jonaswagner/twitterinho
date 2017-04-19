ede31dd483cd77a02e331f7bc24f01260f9042dc
Flavio Keller <flavio.keller@uzh.ch>
2017-04-19 23:04:27 +0200

    changed configs for server deployment

--- server/src/main/java/ch/uzh/ase/NLP.java
+++ server/src/main/java/ch/uzh/ase/NLP.java
@@ -1,46 +1,46 @@
-package ch.uzh.ase;
-
-
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.CoreMap;
-
-import java.util.Properties;
-
-public class NLP {
-
-    static StanfordCoreNLP pipeline;
-
-    public static void init() {
-        //pipeline = new StanfordCoreNLP("main/java/ch/uzh/ase/MyPropFile.properties");
-        Properties properties = new Properties();
-        properties.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
-        pipeline = new StanfordCoreNLP(properties);
-    }
-
-    public static int findSentiment(String tweet) {
-
-        int mainSentiment = 0;
-        if (tweet != null && tweet.length() > 0) {
-            int longest = 0;
-            Annotation annotation = pipeline.process(tweet);
-            for (CoreMap sentence : annotation
-                    .get(CoreAnnotations.SentencesAnnotation.class)) {
-                Tree tree = sentence
-                        .get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
-                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-                String partText = sentence.toString();
-                if (partText.length() > longest) {
-                    mainSentiment = sentiment;
-                    longest = partText.length();
-                }
-
-            }
-        }
-        return mainSentiment;
-    }
-}
+//package ch.uzh.ase;
+//
+//
+//import edu.stanford.nlp.ling.CoreAnnotations;
+//import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+//import edu.stanford.nlp.pipeline.Annotation;
+//import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+//import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+//import edu.stanford.nlp.trees.Tree;
+//import edu.stanford.nlp.util.CoreMap;
+//
+//import java.util.Properties;
+//
+//public class NLP {
+//
+//    static StanfordCoreNLP pipeline;
+//
+//    public static void init() {
+//        //pipeline = new StanfordCoreNLP("main/java/ch/uzh/ase/MyPropFile.properties");
+//        Properties properties = new Properties();
+//        properties.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+//        pipeline = new StanfordCoreNLP(properties);
+//    }
+//
+//    public static int findSentiment(String tweet) {
+//
+//        int mainSentiment = 0;
+//        if (tweet != null && tweet.length() > 0) {
+//            int longest = 0;
+//            Annotation annotation = pipeline.process(tweet);
+//            for (CoreMap sentence : annotation
+//                    .get(CoreAnnotations.SentencesAnnotation.class)) {
+//                Tree tree = sentence
+//                        .get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+//                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+//                String partText = sentence.toString();
+//                if (partText.length() > longest) {
+//                    mainSentiment = sentiment;
+//                    longest = partText.length();
+//                }
+//
+//            }
+//        }
+//        return mainSentiment;
+//    }
+//}
--- server/src/main/java/ch/uzh/ase/What2Think.java
+++ server/src/main/java/ch/uzh/ase/What2Think.java
@@ -1,48 +1,48 @@
-package ch.uzh.ase;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.util.ArrayList;
-
-public class What2Think {
-
-    public static void main(String[] args) {
-
-        String topic = "zurich";
-        ArrayList<String> tweets = null;
-
-        File file = new File("tweets.txt");
-        final FileWriter fw;
-        final BufferedWriter bw;
-
-        try {
-
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            fw = new FileWriter(file.getAbsoluteFile());
-            bw = new BufferedWriter(fw);
-            tweets = TweetManager.getTweets(topic);
-
-            NLP.init();
-            for (String tweet : tweets) {
-                bw.write(NLP.findSentiment(tweet) + ": " + tweet);
-                bw.newLine();
-            }
-
-            bw.flush();
-            bw.close();
-            fw.close();
-
-
-            /*for (String tweet : tweets) {
-                System.out.println(tweet + " : " + NLP.findSentiment(tweet));
-            }*/
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-}
+//package ch.uzh.ase;
+//
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.util.ArrayList;
+//
+//public class What2Think {
+//
+//    public static void main(String[] args) {
+//
+//        String topic = "zurich";
+//        ArrayList<String> tweets = null;
+//
+//        File file = new File("tweets.txt");
+//        final FileWriter fw;
+//        final BufferedWriter bw;
+//
+//        try {
+//
+//            if (!file.exists()) {
+//                file.createNewFile();
+//            }
+//
+//            fw = new FileWriter(file.getAbsoluteFile());
+//            bw = new BufferedWriter(fw);
+//            tweets = TweetManager.getTweets(topic);
+//
+//            NLP.init();
+//            for (String tweet : tweets) {
+//                bw.write(NLP.findSentiment(tweet) + ": " + tweet);
+//                bw.newLine();
+//            }
+//
+//            bw.flush();
+//            bw.close();
+//            fw.close();
+//
+//
+//            /*for (String tweet : tweets) {
+//                System.out.println(tweet + " : " + NLP.findSentiment(tweet));
+//            }*/
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//}
